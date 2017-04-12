0539cd8ee98f32d95a53a1e62b83ff99cea9bec1
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-04-13 04:00:02 +0500

    upd travis config and build scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-
+# build Domain
+dotnet restore src/Core/NetCoreCqrsEsSample.Domain/
 dotnet build src/Core/NetCoreCqrsEsSample.Domain/
+# build Commands
+dotnet restore src/Core/NetCoreCqrsEsSample.Commands/
 dotnet build src/Core/NetCoreCqrsEsSample.Commands/
+# build Events
+dotnet restore src/Core/NetCoreCqrsEsSample.Events/
 dotnet build src/Core/NetCoreCqrsEsSample.Events/
