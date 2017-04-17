4e759b658fab383b449a9c108da1aa3b9e7dd38b
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-04-17 22:35:59 +0500

    repo implementation

--- src/Services/NetCoreCqrsEsSample.Services.CounterService/CounterService.cs
+++ src/Services/NetCoreCqrsEsSample.Services.CounterService/CounterService.cs
@@ -1,10 +1,17 @@
 using NetCoreCqrsEsSample.Domain.Models;
+using NetCoreCqrsEsSample.Domain.Repositories;
 using NetCoreCqrsEsSample.Domain.Services;
 
 namespace NetCoreCqrsEsSample.Services.CounterService
 {
     public class CounterService: ICounterService
     {
-        public Counter Get() => new Counter();
+        private readonly ICounterRepo _repo;
+
+        public CounterService(ICounterRepo repo)
+        {
+            _repo = repo;
+        }
+        public Counter Get() => _repo.Get();
     }
 }
