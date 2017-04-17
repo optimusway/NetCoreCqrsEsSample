0eceb2a7a5b16c64d0cb69bc989c81e83188a289
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-04-17 23:46:59 +0500

    rename service project; fix build script

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ dotnet build src/Core/NetCoreCqrsEsSample.Commands/
 dotnet restore src/Core/NetCoreCqrsEsSample.Events/
 dotnet build src/Core/NetCoreCqrsEsSample.Events/
 # build Events
-dotnet restore src/Infrastructure/NetCoreCqrsEsSample.Commands/
-dotnet build src/Infrastructure/NetCoreCqrsEsSample.Commands/
+dotnet restore src/Infrastructure/NetCoreCqrsEsSample.Infrastructure.Commands/
+dotnet build src/Infrastructure/NetCoreCqrsEsSample.Infrastructure.Commands/
 # build Events
 dotnet restore src/Infrastructure/NetCoreCqrsEsSample.Data/
 dotnet build src/Infrastructure/NetCoreCqrsEsSample.Data/
 # build Events
-dotnet restore src/Services/NetCoreCqrsEsSample.Services.CounterService/
-dotnet build src/Services/NetCoreCqrsEsSample.Services.CounterService/
+dotnet restore src/Services/NetCoreCqrsEsSample.Service.CounterService/
+dotnet build src/Services/NetCoreCqrsEsSample.Service.CounterService/
