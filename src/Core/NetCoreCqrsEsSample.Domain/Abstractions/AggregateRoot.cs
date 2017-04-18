19fc64f73c9b7b9003dfa4e429f7cdbd22c3a8b0
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-04-19 01:51:24 +0500

    add event appliers to the aggregate root

--- src/Core/NetCoreCqrsEsSample.Domain/Abstractions/AggregateRoot.cs
+++ src/Core/NetCoreCqrsEsSample.Domain/Abstractions/AggregateRoot.cs
@@ -1,11 +1,38 @@
 using System;
+using System.Collections.Generic;
+using NetCoreCqrsEsSample.Events;
 
 namespace NetCoreCqrsEsSample.Domain.Abstractions
 {
     public abstract class AggregateRoot
     {
+        private readonly List<IEvent> _changes = new List<IEvent>();
+
         public Guid Id { get; protected set; }
 
         public int Version { get; protected set; }
+
+        public IEnumerable<IEvent> GetUncommittedChanges() => _changes;
+
+        public void MarkChangesAsCommited() => _changes.Clear();
+
+        public void LoadFromHistory(IEnumerable<IEvent> events)
+        {
+            foreach (var e in events)
+            {
+                ApplyChange(e, false);
+            }
+        }
+
+        protected void ApplyChange(IEvent @event) => ApplyChange(@event, true);
+
+        private void ApplyChange(IEvent @event, bool isNew)
+        {
+            ((dynamic)this).Apply(@event);
+            if (isNew)
+            {
+                _changes.Add(@event);
+            }
+        }
     }
 }
