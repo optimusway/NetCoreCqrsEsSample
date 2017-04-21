51f0c83ab37aa622e8de5fef766517681c27c816
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-04-21 13:28:53 +0500

    in-memory event store impl

--- src/Core/NetCoreCqrsEsSample.Events/IEvent.cs
+++ src/Core/NetCoreCqrsEsSample.Events/IEvent.cs
@@ -2,6 +2,6 @@ namespace NetCoreCqrsEsSample.Events
 {
     public interface IEvent
     {
-         
+        int Version { get; set; }
     }
 }
--- src/Infrastructure/NetCoreCqrsEsSample.Data/EventStore/InMemoryEventStore.cs
+++ src/Infrastructure/NetCoreCqrsEsSample.Data/EventStore/InMemoryEventStore.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using NetCoreCqrsEsSample.Domain.EventStore;
 using NetCoreCqrsEsSample.Events;
 
@@ -7,14 +8,94 @@ namespace NetCoreCqrsEsSample.Data.EventStore
 {
     public class InMemoryEventStore : IEventStore
     {
-        public IEnumerable<IEvent> GetEvents(Guid aggregateId)
+        private readonly Dictionary<Guid, List<EventDescriptor>> _store = new Dictionary<Guid, List<EventDescriptor>>();
+        private readonly IEventDispatcher _dispatcher;
+
+        private struct EventDescriptor
         {
-            throw new NotImplementedException();
+            public readonly Guid Id;
+            public readonly IEvent EventData;
+            public readonly int Version;
+
+            public EventDescriptor(Guid id, IEvent eventData, int version)
+            {
+                this.Id = id;
+                this.EventData = eventData;
+                this.Version = version;
+
+            }
         }
 
-        public void Save(Guid id, IEnumerable<IEvent> enumerable, int expectedVersion)
+        public InMemoryEventStore(IEventDispatcher dispatcher)
         {
-            throw new NotImplementedException();
+            this._dispatcher = dispatcher;
+        }
+
+        public IEnumerable<IEvent> GetEvents(Guid aggregateId)
+        {
+            List<EventDescriptor> events;
+            if (!_store.TryGetValue(aggregateId, out events))
+            {
+                throw new AggregateNotFoundException();
+            }
+
+            return events.Select(e => e.EventData);
+        }
+
+        public void Save(Guid id, IEnumerable<IEvent> events, int expectedVersion)
+        {
+            List<EventDescriptor> eventDescriptors;
+            if (!_store.TryGetValue(id, out eventDescriptors))
+            {
+                eventDescriptors = new List<EventDescriptor>();
+                _store.Add(id, eventDescriptors);
+            }
+            else if (eventDescriptors.Last().Version != expectedVersion && expectedVersion != -1)
+            {
+                throw new ConcurencyException();
+            }
+
+            var i = expectedVersion;
+
+            foreach (var e in events)
+            {
+                e.Version = ++i;
+                eventDescriptors.Add(
+                    new EventDescriptor(id, e, i)
+                );
+
+                _dispatcher.DispatchAsync<IEvent>(e);
+            }
+        }
+    }
+
+    internal class AggregateNotFoundException : Exception
+    {
+        public AggregateNotFoundException()
+        {
+        }
+
+        public AggregateNotFoundException(string message) : base(message)
+        {
+        }
+
+        public AggregateNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    internal class ConcurencyException : Exception
+    {
+        public ConcurencyException()
+        {
+        }
+
+        public ConcurencyException(string message) : base(message)
+        {
+        }
+
+        public ConcurencyException(string message, Exception innerException) : base(message, innerException)
+        {
         }
     }
 }
