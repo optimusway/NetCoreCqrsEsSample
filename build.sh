d07b38eac5ffe0744e51591f9b92a99c868cd0c1
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-04-24 12:32:23 +0500

    revert custom build instructions back

--- build.sh
+++ build.sh
@@ -1,2 +1,25 @@
-# build sln
-dotnet build NetCoreCqrsEsSample.sln
+# build Domain
+dotnet restore src/Core/NetCoreCqrsEsSample.Domain/
+dotnet build src/Core/NetCoreCqrsEsSample.Domain/
+# build Commands
+dotnet restore src/Core/NetCoreCqrsEsSample.Commands/
+dotnet build src/Core/NetCoreCqrsEsSample.Commands/
+# build Events
+dotnet restore src/Core/NetCoreCqrsEsSample.Events/
+dotnet build src/Core/NetCoreCqrsEsSample.Events/
+# build Commands
+dotnet restore src/Infrastructure/NetCoreCqrsEsSample.Infrastructure.Commands/
+dotnet build src/Infrastructure/NetCoreCqrsEsSample.Infrastructure.Commands/
+# build Data
+dotnet restore src/Infrastructure/NetCoreCqrsEsSample.Data/
+dotnet build src/Infrastructure/NetCoreCqrsEsSample.Data/
+# build CounterService
+dotnet restore src/Services/NetCoreCqrsEsSample.Service.CounterService/
+dotnet build src/Services/NetCoreCqrsEsSample.Service.CounterService/
+# build ServiceHost
+dotnet restore src/Services/NetCoreCqrsEsSample.Service.Host/
+dotnet build src/Services/NetCoreCqrsEsSample.Service.Host/
+
+# build tests
+dotnet restore tests/NetCoreCqrsEsSample.Tests/NetCoreCqrsEsSample.Tests.csproj
+dotnet build tests/NetCoreCqrsEsSample.Tests/NetCoreCqrsEsSample.Tests.csproj
