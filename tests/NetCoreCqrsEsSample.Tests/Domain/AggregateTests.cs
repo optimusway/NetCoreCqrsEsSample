e2d8a84930d95381999031fbc3e948075dfcc6c2
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-05-04 09:53:46 +0500

    Add tests for restoring the aggregate from events

---
+++ tests/NetCoreCqrsEsSample.Tests/Domain/AggregateTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NetCoreCqrsEsSample.Domain.Models;
+using Xunit;
+
+namespace NetCoreCqrsEsSample.Tests.Domain
+{
+    public class AggregateTests
+    {
+        [Fact]
+        public void Should_load_aggregate_from_events_correctly()
+        {
+            // arrange
+            var counter = new Counter();
+            var newCounter = new Counter();
+
+            // act
+            counter.Increment();
+            counter.Increment();
+            counter.Decrement();
+            counter.Increment();
+            newCounter.LoadFromHistory(counter.GetUncommittedChanges());
+
+            // assert
+            counter.Value.Should().Be(2);
+            newCounter.Value.Should().Be(2);
+        }
+    }
+}
